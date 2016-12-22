124d3f8e12e71201f4295197fe9c90e196e1cf78
Valeri Karpov <valkar207@gmail.com>
2016-12-21 23:51:42 -0700

    initial PoC

---
+++ index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const { Observable } = require('rxjs/Observable');
+require('rxjs/add/observable/fromPromise');
+require('rxjs/add/operator/catch');
+require('rxjs/add/operator/map');
+require('rxjs/add/operator/share');
+
+module.exports = function(gen) {
+  if (typeof gen === 'function') {
+    gen = gen();
+  }
+  if (typeof gen.next !== 'function') {
+    throw new Error('Parameter does not look like a generator');
+  }
+
+  const observable = new Observable(observer => {
+    onComplete();
+
+    function onComplete(res) {
+      let ret;
+      try {
+        ret = gen.next(res);
+      } catch (e) {
+        return observer.error(e);
+      }
+      next(ret);
+      return null;
+    }
+
+    function onError(err) {
+      let ret;
+      try {
+        ret = gen.throw(err);
+      } catch (e) {
+        return observer.error(e);
+      }
+      next(ret);
+    }
+
+    function next(ret) {
+      if (ret.done) {
+        if (ret.value !== void 0) {
+          observer.next(toObservable(ret.value));
+        }
+        return observer.complete();
+      }
+      const value = toObservable(ret.value);
+      if (value) {
+        observer.next(value);
+
+        const v = [];
+        value.subscribe(
+          res => { v.push(res); },
+          error => onError(error),
+          () => {
+            if (v.length === 0) {
+              onComplete(void 0);
+            } else if (v.length === 1) {
+              onComplete(v[0]);
+            } else {
+              onComplete(v);
+            }
+          }
+        );
+
+        return;
+      }
+      return onError(new TypeError('You may only yield a promise '
+          + 'but the following object was passed: "' + String(ret.value) + '"'));
+    }
+  });
+
+  return observable.share();
+}
+
+function toObservable(obj) {
+  if (obj) {
+    if (typeof obj.subscribe === 'function') {
+      // Observable
+      return obj;
+    } else if (typeof obj.then === 'function') {
+      // Promise
+      return Observable.fromPromise(obj);
+    } else if (typeof obj === 'function') {
+      // Thunk
+      return thunkToObservable(obj);
+    }
+  }
+}
+
+function thunkToObservable(fn) {
+  return new Observable(observer => {
+    try {
+      fn(function(error, res) {
+        if (error) {
+          return observer.error(error);
+        }
+        observer.next(res);
+        observer.complete();
+      });
+    } catch (error) {
+      observer.error(error);
+    }
+  });
+}
---
+++ test/examples.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const { Observable } = require('rxjs/Observable');
+const assert = require('assert');
+const axl = require('../');
+
+require('rxjs/add/observable/of');
+
+describe('axl', function() {
+  it('works', function(done) {
+    let isDone = false;
+    let opCount = 0;
+    axl(function*() {
+      const res = yield new Promise(resolve => setTimeout(() => resolve('hello'), 50))
+      assert.equal(res, 'hello');
+      const res2 = yield cb => setTimeout(() => cb(null, 'test'), 50);
+      assert.equal(res2, 'test');
+      isDone = true;
+    }).subscribe(
+      op$ => {
+        op$.subscribe(v => ++opCount);
+      },
+      error => {
+        done(error);
+      },
+      () => {
+        assert.ok(done);
+        assert.equal(opCount, 2);
+        done();
+      }
+    );
+  });
+
+  it('handling errors', function(done) {
+    let isDone = false;
+
+    axl(function*() {
+      yield new Promise((_, reject) => reject(new Error('hello')));
+      isDone = true;
+    }).
+    catch(error => {
+      throw new Error('Test2');
+    }).
+    subscribe(
+      op$ => {
+        op$.subscribe(v => ++opCount);
+      },
+      error => {
+        assert.equal(error.message, 'Test2');
+        assert.ok(!isDone);
+        done();
+      },
+      () => {
+        done(new Error('Should not complete'));
+      }
+    );
+  });
+});
